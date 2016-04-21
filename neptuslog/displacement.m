6f401be62d95ba353bbcf3c16da07c703f9da2c7
Kjetil Soerboe <kjetilhs@stud.ntnu.no>
2016-04-21 11:21:13 +0200

    Created a displacement function for coordinating ref system

---
+++ neptuslog/displacement.m
@@ -0,0 +1,4 @@
+function[n,e,d] = displacement(lat,lon,height,ref_lat,ref_lon,ref_height,x,y,z)
+[temp_lat,temp_lon,temp_height] = ned2geodetic(x,y,z,lat,lon,height,wgs84Ellipsoid);
+[n,e,d] = geodetic2ned(temp_lat,temp_lon,temp_height,ref_lat,ref_lon,ref_height,wgs84Ellipsoid);
+end
