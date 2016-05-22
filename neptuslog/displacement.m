ca8cb4c7a60c9c746bbd660701ba607798e8d453
Kjetil Soerboe <kjetilhs@stud.ntnu.no>
2016-05-22 11:29:55 +0200

    Fikser plot

--- neptuslog/displacement.m
+++ neptuslog/displacement.m
@@ -1,4 +1,5 @@
 function[n,e,d] = displacement(lat,lon,height,ref_lat,ref_lon,ref_height,x,y,z)
+% Finds the offset from ref to lat,lon height with offset x,y,z
 [temp_lat,temp_lon,temp_height] = ned2geodetic(x,y,z,lat,lon,height,wgs84Ellipsoid);
 [n,e,d] = geodetic2ned(temp_lat,temp_lon,temp_height,ref_lat,ref_lon,ref_height,wgs84Ellipsoid);
 end
